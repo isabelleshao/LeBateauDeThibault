559c10ed72ea23c28c70de90ec6cb3fed3268a70
isabelleshao <47239377+isabelleshao@users.noreply.github.com>
2022-02-06 18:13:13 +0100

    panier screen

--- src/component/ModalPicker.js
+++ src/component/ModalPicker.js
@@ -1,109 +1,105 @@
-import React from "react"
-import { useSelector, useDispatch } from 'react-redux'
-import { addToBasket, removeFromBasket, setQuantity} from "../component/BasketSlice"
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addToBasket,
+  removeFromBasket,
+  setQuantity,
+} from "../component/BasketSlice";
 
-import{
-    StyleSheet,
-    Text,
-    View,
-    TouchableOpacity,
-    Dimensions,
-    ScrollView,
-} from "react-native"
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Dimensions,
+  ScrollView,
+} from "react-native";
 
-const OPTIONS = ['1', '2', '3', '4', '5'];
-const WIDTH = Dimensions.get('window').width;
-const HEIGHT = Dimensions.get('window').height;
+const OPTIONS = ["1", "2", "3", "4", "5"];
+const WIDTH = Dimensions.get("window").width;
+const HEIGHT = Dimensions.get("window").height;
 
 const ModalPicker = (props) => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const onPressItem = (option) => {
+    props.changeModalVisibility(false);
+    props.setData(option);
 
-    const onPressItem = (option) => {
-        props.changeModalVisibility(false);
-        props.setData(option);
+    dispatch(setQuantity({ selectedId: props.selectedId, option }));
+  };
 
-        dispatch(setQuantity({selectedId:(props.selectedId),option}));
-    }
-
-    const option = OPTIONS.map((item, index) => {
-        return(
-            
-            <TouchableOpacity
-                style={styles.option}
-                key={index}
-                onPress={() => onPressItem(item)}
-            >
-                <Text style={styles.text}>
-                    {item}
-                </Text>
-            </TouchableOpacity>
-        )
-    })
-
+  const option = OPTIONS.map((item, index) => {
     return (
-        <TouchableOpacity
-            onPress={() => props.changeModalVisibility(false)}
-            style={styles.container}
-        >
-            <View style={[styles.modal, {width: WIDTH-20, height: HEIGHT/2}]}>
-            <Text style={styles.titre}> Selectionner la quantité </Text>
-                <ScrollView>
-                    {option}
-                    <TouchableOpacity
-                        style={styles.option}
-                        onPress={() => {
-                            dispatch(addToBasket(props.selectedId));
-                            props.changeModalVisibility(false);
-                        }}
-                    >
-                        <Text style={styles.text}>
-                            {"Ajouter 1"}
-                        </Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={styles.option}
-                        onPress={() => {dispatch(removeFromBasket(props.selectedId));
-                            props.changeModalVisibility(false);
-                        }}
-                    >
-                        <Text style={styles.text}>
-                            {"Enlever 1"}
-                        </Text>
-                    </TouchableOpacity>
-                </ScrollView>
-            </View>
-        </TouchableOpacity>
-    ) 
-}
+      <TouchableOpacity
+        style={styles.option}
+        key={index}
+        onPress={() => onPressItem(item)}
+      >
+        <Text style={styles.text}>{item}</Text>
+      </TouchableOpacity>
+    );
+  });
+
+  return (
+    <TouchableOpacity
+      onPress={() => props.changeModalVisibility(false)}
+      style={styles.container}
+    >
+      <View style={[styles.modal, { width: WIDTH - 20, height: HEIGHT / 2 }]}>
+        <Text style={styles.titre}> Selectionner la quantité </Text>
+        <ScrollView>
+          {option}
+          <TouchableOpacity
+            style={styles.option}
+            onPress={() => {
+              dispatch(addToBasket(props.selectedId));
+              props.changeModalVisibility(false);
+            }}
+          >
+            <Text style={styles.text}>{"Ajouter 1"}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.option}
+            onPress={() => {
+              dispatch(removeFromBasket(props.selectedId));
+              props.changeModalVisibility(false);
+            }}
+          >
+            <Text style={styles.text}>{"Enlever 1"}</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center"
-    },
-    modal: {
-        backgroundColor: "white",
-        borderRadius: 10
-    },
-    option: {
-        borderTopWidth: 1,
-        alignItems: "flex-start"
-    },
-    text: {
-        margin: 20,
-        fontSize: 20,
-        fontWeight: "bold"
-    },
-    titre: {
-        fontSize: 30,
-        color: "black",
-        fontStyle: "italic",
-        marginBottom: 30,
-        marginTop: 20,
-        alignSelf: "center",
-      },
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  modal: {
+    backgroundColor: "white",
+    borderRadius: 10,
+  },
+  option: {
+    borderTopWidth: 1,
+    alignItems: "flex-start",
+  },
+  text: {
+    margin: 20,
+    fontSize: 18,
+  },
+  titre: {
+    fontSize: 30,
+    color: "black",
+    fontStyle: "italic",
+    marginBottom: 30,
+    marginTop: 20,
+    alignSelf: "center",
+  },
+});
 
-export {ModalPicker}
+export { ModalPicker };
--- src/screen/PanierScreen.js
+++ src/screen/PanierScreen.js
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import { useSelector, useDispatch } from 'react-redux'
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { ModalPicker } from "../component/ModalPicker";
 import data from "../assets/data.json";
 
@@ -13,6 +13,7 @@ import {
   TouchableOpacity,
   ScrollView,
   Modal,
+  Alert,
 } from "react-native";
 import globalStyles from "../component/GlobalStyle";
 
@@ -21,27 +22,47 @@ const PanierScreen = () => {
   const panier = useSelector((state) => state.basket.basket);
   const keys = Object.keys(panier);
 
-  const [chooseData, setChooseData] = useState('Modifier la quantité ')
-  const [isModalVisible, setisModalVisible] = useState(false)
+  const [chooseData, setChooseData] = useState("Modifier la quantité ");
+  const [isModalVisible, setisModalVisible] = useState(false);
   const [selectedId, setId] = useState(0);
 
-  const filteredData = data.filter((produit) => (keys.includes(String(produit.id)) && panier[produit.id] > 0) )
+  const filteredData = data.filter(
+    (produit) => keys.includes(String(produit.id)) && panier[produit.id] > 0
+  );
   const changeModalVisibility = (bool) => {
     setisModalVisible(bool);
-  }
+  };
 
   const setData = (option) => {
     setChooseData(option);
-  }
+  };
   const getTotal = () => {
     var total = 0;
-    filteredData.forEach(element => 
-      total += Number(element.price*panier[element.id])
-    )
+    filteredData.forEach(
+      (element) => (total += Number(element.price * panier[element.id]))
+    );
     return total;
-    }
+  };
 
   var date = new Date().toLocaleDateString();
+
+  if (filteredData.length === 0) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <ImageBackground
+          source={require("../assets/img/background.png")}
+          resizeMode="cover"
+          style={{ flex: 1, justifyContent: "center" }}
+        >
+          <ScrollView>
+            <Text style={styles.panierVide}>Votre panier est vide </Text>
+          </ScrollView>
+        </ImageBackground>
+      </SafeAreaView>
+    );
+  } else {
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <ImageBackground
@@ -49,54 +70,88 @@ const PanierScreen = () => {
         resizeMode="cover"
         style={{ flex: 1, justifyContent: "center" }}
       >
-        <ScrollView>
-          {filteredData.map((filteredProduit) => (
-            <View key={filteredProduit.id}>
-              <TouchableOpacity
-                style={styles.boutonSoloStyle}
-                onPress={() => {setId(filteredProduit.id);
-                  setisModalVisible(true);
-                }}
-              >
+        <ScrollView style={styles.test}>
+          <View>
+            {filteredData.map((filteredProduit) => (
+              <View key={filteredProduit.id}>
+                <TouchableOpacity
+                  style={styles.boutonSoloStyle}
+                  onPress={() => {
+                    setId(filteredProduit.id);
+                    setisModalVisible(true);
+                  }}
+                >
                   <Modal
                     transparent={true}
-                    animationType='fade'
+                    animationType="fade"
                     visible={isModalVisible}
                   >
-                  <ModalPicker
-                    style={{backgroundColor: "white"}}
-                    changeModalVisibility = {changeModalVisibility}
-                    setData={setData}
-                    selectedId={selectedId}
+                    <ModalPicker
+                      style={{ backgroundColor: "white" }}
+                      changeModalVisibility={changeModalVisibility}
+                      setData={setData}
+                      selectedId={selectedId}
+                    />
+                  </Modal>
+                  <Image
+                    style={styles.icon}
+                    source={require("../assets/img/poulpe.png")}
                   />
-                </Modal>
-                <Image
-                  style={styles.icon}
-                  source={require("../assets/img/poulpe.png")}
-                />
-                <View>
-                  <Text style={globalStyles.bold}>{filteredProduit.name}</Text>
-                  <Text>
-                    {" "}
-                    {panier[filteredProduit.id]}x{filteredProduit.price} € ={" "}
-                    {filteredProduit.price * panier[filteredProduit.id]}€{" "}
-                  </Text>
-                </View>
-              </TouchableOpacity>
-            </View>
-          ))}
+                  <View>
+                    <Text style={globalStyles.bold}>
+                      {filteredProduit.name}
+                    </Text>
+                    <Text>
+                      {" "}
+                      {panier[filteredProduit.id]}x{filteredProduit.price} € ={" "}
+                      {filteredProduit.price * panier[filteredProduit.id]}€{" "}
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            ))}
+          </View>
+          <View style={styles.total}>
+            <TouchableOpacity onPress={() => console.log("oui")}>
+              <View>
+                <Text style={styles.totalText}>Total : {getTotal()} €</Text>
+                <Text style={styles.commandeText}>
+                  Lieu de Livraison (choisir) : {date}
+                </Text>
+                <Text style={styles.commandeText}>
+                  Date de Livraison : {date}
+                </Text>
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={() =>
+                Alert.alert(
+                  "Envoyer votre commande?",
+                  `Envoyer votre commande de ` + getTotal() + ` € à Thibault?`,
+                  [
+                    {
+                      text: "Non",
+                      onPress: () => console.log("Cancel Pressed"),
+                      style: "cancel",
+                    },
+                    {
+                      text: "Oui",
+                      onPress: () => Alert.alert("Votre commande est passée"),
+                    },
+                  ],
+                  {
+                    cancelable: true,
+                  }
+                )
+              }
+            >
+              <View>
+                <Text style={styles.totalText}>Valider</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
         </ScrollView>
-        <TouchableOpacity style={styles.container}
-                  onPress={() => console.log("oui")}>
-          <View>
-            <Text> Total : {getTotal()} </Text>     
-          </View>
-          <View>
-          </View>
-          <View>
-            <Text>{date}</Text>
-          </View>
-        </TouchableOpacity>
       </ImageBackground>
     </SafeAreaView>
   );
@@ -104,6 +159,26 @@ const PanierScreen = () => {
 
 const styles = StyleSheet.create({
   container: { flex: 1 },
+
+  total: {
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
+  },
+  totalText: {
+    fontSize: 30,
+    marginHorizontal: 10,
+    textAlign: "right",
+    color: "white",
+  },
+  commandeText: {
+    fontSize: 20,
+    marginHorizontal: 10,
+    color: "white",
+  },
+
+  panierVide: {
+    padding: 20,
+    fontSize: 20,
+  },
   icon: {
     height: 50,
     width: 50,
@@ -119,7 +194,9 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
   },
 
-  
+  validerButton: {
+    fontSize: 30,
+  },
 });
 
 export default PanierScreen;
