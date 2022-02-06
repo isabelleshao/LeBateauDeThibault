75cff010f27a4519646d7a9b001ec91a31ee584f
isabelleshao <47239377+isabelleshao@users.noreply.github.com>
2022-02-06 18:31:11 +0100

    Update PanierScreen.js

--- src/screen/PanierScreen.js
+++ src/screen/PanierScreen.js
@@ -48,79 +48,159 @@ const PanierScreen = () => {
     Alert.alert("Validation", "Commande validée !");
 
   const createTwoButtonAlert = () =>
-    Alert.alert("Confirmation", "Confirmez-vous la commande ?", [
+    Alert.alert(
+      "Envoyer votre commande?",
+      `Envoyer votre commande de ` + getTotal() + ` € à Thibault?`,
+      [
+        {
+          text: "Non",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel",
+        },
+        {
+          text: "Oui",
+          onPress: () => createValidationAlert,
+        },
+      ],
       {
-        text: "Annuler",
-        onPress: () => console.log("Annulation"),
-      },
-      { text: "Valider", onPress: createValidationAlert },
-    ]);
+        cancelable: true,
+      }
+    );
 
   var date = new Date().toLocaleDateString();
-  return (
-    <SafeAreaView style={styles.container}>
-      <ImageBackground
-        source={require("../assets/img/background.png")}
-        resizeMode="cover"
-        style={{ flex: 1, justifyContent: "center" }}
-      >
-        <ScrollView>
-          {filteredData.map((filteredProduit) => (
-            <View key={filteredProduit.id}>
-              <TouchableOpacity
-                style={styles.boutonSoloStyle}
-                onPress={() => {
-                  setId(filteredProduit.id);
-                  setisModalVisible(true);
-                }}
-              >
-                <Modal
-                  transparent={true}
-                  animationType="fade"
-                  visible={isModalVisible}
+
+  if (filteredData.length === 0) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <ImageBackground
+          source={require("../assets/img/background.png")}
+          resizeMode="cover"
+          style={{ flex: 1, justifyContent: "center" }}
+        >
+          <ScrollView>
+            <Text style={styles.panierVide}>Votre panier est vide </Text>
+          </ScrollView>
+        </ImageBackground>
+      </SafeAreaView>
+    );
+  } else {
+    return (
+      <SafeAreaView style={styles.container}>
+        <ImageBackground
+          source={require("../assets/img/background.png")}
+          resizeMode="cover"
+          style={{ flex: 1, justifyContent: "center" }}
+        >
+          <ScrollView>
+            {filteredData.map((filteredProduit) => (
+              <View key={filteredProduit.id}>
+                <TouchableOpacity
+                  style={styles.boutonSoloStyle}
+                  onPress={() => {
+                    setId(filteredProduit.id);
+                    setisModalVisible(true);
+                  }}
                 >
-                  <ModalPicker
-                    style={{ backgroundColor: "white" }}
-                    changeModalVisibility={changeModalVisibility}
-                    setData={setData}
-                    selectedId={selectedId}
+                  <Modal
+                    transparent={true}
+                    animationType="fade"
+                    visible={isModalVisible}
+                  >
+                    <ModalPicker
+                      style={{ backgroundColor: "white" }}
+                      changeModalVisibility={changeModalVisibility}
+                      setData={setData}
+                      selectedId={selectedId}
+                    />
+                  </Modal>
+                  <Image
+                    style={styles.icon}
+                    source={require("../assets/img/poulpe.png")}
                   />
-                </Modal>
-                <Image
-                  style={styles.icon}
-                  source={require("../assets/img/poulpe.png")}
-                />
+                  <View>
+                    <Text style={globalStyles.bold}>
+                      {filteredProduit.name}
+                    </Text>
+                    <Text>
+                      {panier[filteredProduit.id]}x{filteredProduit.price} € ={" "}
+                      {filteredProduit.price * panier[filteredProduit.id]}€{" "}
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            ))}
+
+            <View style={styles.total}>
+              <TouchableOpacity onPress={() => console.log("oui")}>
                 <View>
-                  <Text style={globalStyles.bold}>{filteredProduit.name}</Text>
-                  <Text>
-                    {" "}
-                    {panier[filteredProduit.id]}x{filteredProduit.price} € ={" "}
-                    {filteredProduit.price * panier[filteredProduit.id]}€{" "}
+                  <Text style={styles.totalText}>Total : {getTotal()} €</Text>
+                  <Text style={styles.commandeText}>
+                    Lieu de Livraison (choisir) : {date}
+                  </Text>
+                  <Text style={styles.commandeText}>
+                    Date de Livraison : {date}
                   </Text>
                 </View>
               </TouchableOpacity>
+
+              <TouchableOpacity
+                onPress={() =>
+                  Alert.alert(
+                    "Envoyer votre commande?",
+                    `Envoyer votre commande de ` +
+                      getTotal() +
+                      ` € à Thibault?`,
+                    [
+                      {
+                        text: "Non",
+                        onPress: () => console.log("Cancel Pressed"),
+                        style: "cancel",
+                      },
+                      {
+                        text: "Oui",
+                        onPress: () => Alert.alert("Votre commande est passée"),
+                      },
+                    ],
+                    {
+                      cancelable: true,
+                    }
+                  )
+                }
+              >
+                <View>
+                  <Text style={styles.totalText}>Valider</Text>
+                </View>
+              </TouchableOpacity>
             </View>
-          ))}
-        </ScrollView>
-        <TouchableOpacity
-          style={styles.container}
-          onPress={createTwoButtonAlert}
-        >
-          <View>
-            <Text> Total : {getTotal()} </Text>
-          </View>
-          <View></View>
-          <View>
-            <Text>{date}</Text>
-          </View>
-        </TouchableOpacity>
-      </ImageBackground>
-    </SafeAreaView>
-  );
+          </ScrollView>
+        </ImageBackground>
+      </SafeAreaView>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
   container: { flex: 1 },
+
+  total: {
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
+  },
+  totalText: {
+    fontSize: 30,
+    marginHorizontal: 10,
+    textAlign: "right",
+    color: "white",
+  },
+  commandeText: {
+    fontSize: 20,
+    marginHorizontal: 10,
+    color: "white",
+  },
+
+  panierVide: {
+    padding: 20,
+    fontSize: 20,
+  },
   icon: {
     height: 50,
     width: 50,
@@ -134,6 +214,10 @@ const styles = StyleSheet.create({
     width: "98%",
     flexDirection: "row",
     justifyContent: "flex-start",
+  },
+
+  validerButton: {
+    fontSize: 30,
   },
 });
 
